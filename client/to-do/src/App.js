2de1ec251ddc0fac3339231616e302500379b20d
Kelvin Kipsang <kelvinkipsang@gmail.com>
2021-02-07 21:45:06 +0300

    chore: [#3] setting up graphql connection

--- client/to-do/src/App.js
+++ client/to-do/src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import "./App.css";
+
+const link = from([
+  new HttpLink({ uri: "http://localhost:4000/graphql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      
+    </ApolloProvider>
   );
 }
 
