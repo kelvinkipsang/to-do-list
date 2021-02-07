c2e2f3ab99e11e9f6b7e5c0e4fce3fbfdde11817
Kelvin Kipsang <kelvinkipsang@gmail.com>
2021-02-07 20:01:02 +0300

    chore:[#2]import schema to server index

--- client/server/index.js
+++ client/server/index.js
@@ -4,15 +4,18 @@ const PORT = 4000;
 const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
 
+const schema = require("./schemas/index");
+
 app.use(cors());
 app.use(express.json());
 app.use(  
   "/graphql",
   graphqlHTTP({
+    schema,
     graphiql: true,
   })
 );
 
 app.listen(PORT, () => {
-  console.log("Server running on localost:4000");
+  console.log("Server running");
 });
